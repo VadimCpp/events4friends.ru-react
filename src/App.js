3b0b4b259a5bee2a5f426061f1d46ff9be217144
Vadim Cpp <vadim.cpp@gmail.com>
2018-12-04 20:03:47 +0200

    Move main view to views folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import MainView from './MainView.js'
+import MainView from './views/MainView.js'
 import './App.css';
 
 class App extends Component {
