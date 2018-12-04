73b59f54291702e903839e187e22a95b1ac9d6d0
Vadim Cpp <vadim.cpp@gmail.com>
2018-12-04 20:02:09 +0200

    Add main view

---
+++ src/MainView.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './MainView.css';
+
+class MainView extends Component {
+  render() {
+    return (
+      <div class="main-view">
+        <div>
+          Events4Friends
+        </div>
+        <div>
+          Events List
+        </div>
+        <div>
+          Button "Ask question"
+        </div>
+        <div>
+          Button "Donate"
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default MainView;
