785e77916ffae23157e35c785eb9ff41bc5506b6
Vadim Cpp <vadim.cpp@gmail.com>
2018-12-04 22:22:29 +0200

    Add buttons

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,20 +1,43 @@
 import React, { Component } from 'react';
 import LogoTitle from '../components/LogoTitle.js'
+import { Button } from 'reactstrap';
 import './MainView.css';
 
 class MainView extends Component {
+  openChat() {    
+    // TODO: put yandex goals here
+    window.location.href = 'https://t.me/events4friends';
+  }
+
+  openDonate() {
+    // TODO: put yandex goals here
+    alert('Not implemented');
+  }
+
   render() {
     return (
       <div class="main-view">
         <LogoTitle />
-        <div>
-          Events List
+        <div class="mt-5">
+          Events List (TODO)
         </div>
-        <div>
-          Button "Ask question"
+        <div class="mt-5">
+          Перейти к обсуждениям в <br />          
+          <Button
+            color="link" 
+            onClick={this.openChat}
+          >
+            telegram-чат
+          </Button>
         </div>
-        <div>
-          Button "Donate"
+        <div class="mt-5">
+          Перейти на страницу <br /> 
+          <Button 
+            color="link"
+            onClick={this.openDonate}
+          >
+            пожертвований
+          </Button>
         </div>
       </div>
     )
