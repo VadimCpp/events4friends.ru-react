f9d19b2cdd63ec9902eedfd5e61349f2cff5d065
Vadim Cpp <vadim.cpp@gmail.com>
2018-12-13 21:50:06 +0200

    Add router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
+
 import { YMInitializer } from 'react-yandex-metrika';
 import MainView from './views/MainView.js'
+import AppRouter from './AppRouter.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App App-fixed-image">
+        <AppRouter />
         <MainView />
-		<div>
-		  /* SNIP */
-		  <YMInitializer accounts={[51441281]} />
-		  /* SNIP */
-		</div>
+    		<div>
+    		  /* SNIP */
+    		  <YMInitializer accounts={[51441281]} />
+    		  /* SNIP */
+    		</div>
       </div>
     );
   }
