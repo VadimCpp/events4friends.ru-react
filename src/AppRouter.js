ec3f3d860edd9afefa6ebcac2c0f2fd1ed649e65
Vadim Cpp <vadim.cpp@gmail.com>
2018-12-16 17:15:19 +0200

    #5

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,13 +2,16 @@ import React from "react";
 import { HashRouter as Router, Route } from "react-router-dom";
 import AboutView from "./views/AboutView.js";
 import MainView from "./views/MainView.js";
+import ScrollToTop from "./components/ScrollToTop.js";
 
 const AppRouter = () => (
   <Router>
-    <div>
-      <Route path="/" exact component={MainView} />
-      <Route path="/about/" component={AboutView} />
-    </div>
+  	<ScrollToTop>
+      <div>
+        <Route path="/" exact component={MainView} />
+        <Route path="/about/" component={AboutView} />
+      </div>
+    </ScrollToTop>
   </Router>
 );
 
