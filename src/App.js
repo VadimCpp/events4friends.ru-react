3540e796e9dff4e922dc2426a64145c968b7000c
Vadim Cpp <vadim.cpp@gmail.com>
2019-04-29 23:21:46 +0200

    Add perfect background image

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App App-fixed-image">
+      <div className="App">
         <LogoTitle />
         <AppRouter />
       </div>
