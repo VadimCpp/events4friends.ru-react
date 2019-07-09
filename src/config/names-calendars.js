918616cba648fc4b81db8f81bef648ccc51efc71
Sunantby <antoha.polischuk@gmail.com>
2019-07-09 12:27:49 +0300

    add config

---
+++ src/config/names-calendars.js
@@ -0,0 +1,10 @@
+const NAMES_CALENDARS = {
+  'vadim.cpp@gmail.com': {
+    name: 'Events For Friends'
+  },
+  'pravonagorod@gmail.com': {
+    name: 'Право на город'
+  }
+};
+
+export default NAMES_CALENDARS;
