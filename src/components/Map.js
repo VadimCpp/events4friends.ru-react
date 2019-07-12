46c33da675a5766ef5db251d36d18b01cc261b1b
reviakin <reviakindi@gmail.com>
2019-07-12 12:28:46 +0300

    closed #30

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import Moment from 'react-moment';
-import { EsriProvider } from 'leaflet-geosearch';
-import { Map as LeafletMap, TileLayer, Marker, Popup } from 'react-leaflet';
-import { NAMES_CALENDARS } from '../config'
-import './map.css';
+import React from "react";
+import Moment from "react-moment";
+import { EsriProvider } from "leaflet-geosearch";
+import { Map as LeafletMap, TileLayer, Marker, Popup } from "react-leaflet";
+import { NAMES_CALENDARS } from "../config";
+import "./map.css";
 
 class Map extends React.Component {
   state = {
     isLoading: true,
     pins: []
-  }
+  };
 
   componentDidUpdate(prevPops, prevState) {
     if (prevPops.pins !== this.props.pins) this.addPins(this.props.allEvents);
@@ -18,30 +18,43 @@ class Map extends React.Component {
     this.addPins(this.props.allEvents);
   }
 
-  addPins = async (events) => {
+  addPins = async events => {
     const provider = new EsriProvider();
 
     for (const event of events) {
       if (!event.location) continue;
 
-      const result = await provider.search({ query: event.location })
-      
+      const result = await provider.search({ query: event.location });
+
       if (!result[0].x || !result[0].y) continue;
 
       this.setState(state => {
-        const { id, description, location, creator: { email }, start: { dateTime } } = event;
+        const {
+          id,
+          description,
+          location,
+          creator: { email },
+          start: { dateTime }
+        } = event;
         const pins = state.pins.slice();
-        pins.push({ id, description, location, email, dateTime, geolocation: { lng: result[0].x, lat: result[0].y }});
+        pins.push({
+          id,
+          description,
+          location,
+          email,
+          dateTime,
+          geolocation: { lng: result[0].x, lat: result[0].y }
+        });
         return { ...state, isLoading: false, pins };
-      })
+      });
     }
-  }
+  };
 
   render() {
     const { pins } = this.state;
-    
+
     if (!pins.length) return null;
-    
+
     return (
       <LeafletMap
         bounds={pins.map(({ geolocation: { lat, lng } }) => ({ lat, lng }))}
@@ -50,30 +63,32 @@ class Map extends React.Component {
         attributionControl={true}
         zoomControl={true}
         doubleClickZoom={true}
-        scrollWheelZoom={true}
+        scrollWheelZoom={false}
         dragging={true}
         animate={true}
-        easeLinearity={0.35}>
-       
-        <TileLayer 
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-        />
+        easeLinearity={0.35}
+      >
+        <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
 
-        {!pins ? null : pins.map(event => {
-          const { lat, lng } = event.geolocation;
+        {!pins
+          ? null
+          : pins.map(event => {
+              const { lat, lng } = event.geolocation;
 
-          return (
-            <Marker key={event.id} position={[lat, lng]}>
-              <Popup>
-                <h5>#{NAMES_CALENDARS[event.email].name}</h5>
-                <p>{event.description.slice(0, 150)}...</p>
-                <p>Место события: {event.location}</p>
-                <p>Начало: <Moment format={'DD MMM HH:mm'}>{event.dateTime}</Moment></p>
-              </Popup>
-            </Marker>
-          );
-        })}
-
+              return (
+                <Marker key={event.id} position={[lat, lng]}>
+                  <Popup>
+                    <h5>#{NAMES_CALENDARS[event.email].name}</h5>
+                    <p>{event.description.slice(0, 150)}...</p>
+                    <p>Место события: {event.location}</p>
+                    <p>
+                      Начало:{" "}
+                      <Moment format={"DD MMM HH:mm"}>{event.dateTime}</Moment>
+                    </p>
+                  </Popup>
+                </Marker>
+              );
+            })}
       </LeafletMap>
     );
   }
