6a7a0fac9e47dbaaabc5512be9b88102f54a22a2
Vadim Cpp <vadim.cpp@gmail.com>
2019-12-19 22:31:16 +0200

    Add logo
    #73

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import HeaderLogo from './Header/header_logo.png';
 import './Header.css';
 
 // eslint-disable-next-line no-unused-vars
@@ -7,6 +8,7 @@ class Header extends Component {
         return(
             <div className="borderbottom">
                 <header className="header">
+                    <img className="header__logo" src={HeaderLogo} alt="Header Logo"/>
                     <h1 className="header__title">events4friends</h1>
                     <h2 className="header__subtitle">События для друзей</h2>
                 </header>
