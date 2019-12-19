de326aa5500a269ad190cbca6a0d0b06f40a6eec
Vadim Cpp <vadim.cpp@gmail.com>
2019-12-19 23:27:27 +0200

    Add two columns to Header
    #73

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import HeaderLogo from './Header/header_logo.png';
 import './Header.css';
 
-// eslint-disable-next-line no-unused-vars
 class Header extends Component {
   render() {
     return(
       <div className="borderbottom">
         <header className="header">
-          <img className="header__logo" src={HeaderLogo} alt="Header Logo"/>
-          <h1 className="header__title">events4friends</h1>
-          <h2 className="header__subtitle">События для друзей</h2>
+          <div className="header__first-col">
+            <img className="header__logo" src={HeaderLogo} alt="Header Logo"/>
+          </div>
+          <div className="header__second-col">
+            <h1 className="header__title">events4friends</h1>
+            <h2 className="header__subtitle">События для друзей</h2>
+          </div>
         </header>
       </div>
     )
