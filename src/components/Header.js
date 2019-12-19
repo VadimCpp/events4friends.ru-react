8b143cc7bd2024b69aac11c3487031a8d6d36a3d
Vadim Cpp <vadim.cpp@gmail.com>
2019-12-19 22:36:31 +0200

    Change Header identation 2 spaces

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,17 @@ import './Header.css';
 
 // eslint-disable-next-line no-unused-vars
 class Header extends Component {
-    render() {
-        return(
-            <div className="borderbottom">
-                <header className="header">
-                    <img className="header__logo" src={HeaderLogo} alt="Header Logo"/>
-                    <h1 className="header__title">events4friends</h1>
-                    <h2 className="header__subtitle">События для друзей</h2>
-                </header>
-            </div>
-        )
-    }
+  render() {
+    return(
+      <div className="borderbottom">
+        <header className="header">
+          <img className="header__logo" src={HeaderLogo} alt="Header Logo"/>
+          <h1 className="header__title">events4friends</h1>
+          <h2 className="header__subtitle">События для друзей</h2>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default Header;
