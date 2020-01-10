d550d643e1bbb4f173672bbdd613a2a21a4b2aec
Vadim Cpp <vadim.cpp@gmail.com>
2020-01-10 20:25:48 +0300

    Create EventsSource.js

---
+++ src/model/EventsSource.js
@@ -0,0 +1,12 @@
+class EventsSource {
+    /**
+     * @param {string} name - название источнока
+     * @param {string} source - ссылка на источник
+     */
+    constructor(name, source,) {    
+      this.name = name;
+      this.source = source;
+    }
+  }
+  
+  export default EventsSource;
