743826c4e3a4e378207e745b55ce090f84f03124
Vadim Cpp <vadim.cpp@gmail.com>
2020-01-13 02:00:02 +0200

    Create Event object for every VK post

--- src/model/EventsSource.js
+++ src/model/EventsSource.js
@@ -1,3 +1,5 @@
+import Event from "./Event";
+
 class EventsSource {
   /**
    * @param {string} name название источника
@@ -9,10 +11,22 @@ class EventsSource {
   }
 
   /**
+   * @param {string} text 
+   * @private
+   * @return {string}
+   */
+  _parseSummary(text) {
+    // https://stackoverflow.com/a/8436533/1775459
+    return text ? text.split('\n')[0] : "Не указано";
+  }
+
+  /**
    * @param {function} cbSuccess функция вызывается при успешном получении данных
    * @param {function} cbError функция вызывается в случае ошибки
    */
   loadEvents(cbSuccess, cbError) {
+    const that = this;
+
     try {
       if (window.VK) {
         const vk = window.VK;
@@ -23,7 +37,7 @@ class EventsSource {
           'wall.get',
           {
             owner_id: -93114971,
-            count: 1,
+            count: 5,
             v: "5.103"
           },
           function(r) {
@@ -32,9 +46,21 @@ class EventsSource {
               
               r.response.items.forEach(item => {
                 console.log(item);
-                // TODO:
-                // const event = this._parseEvent(item);
-                // events.unshift(event);
+
+                const { text } = item;
+
+                const id = "1";
+                const start = "2019-09-14T11:00:00+02:00";
+                const end = "2019-09-14T11:00:00+02:00";
+                const summary = that._parseSummary(text);
+                const description = text;
+                const location = "Не указано";
+                const contact = "https://vk.com/afisha_39";
+                const reference = "Не указано";
+
+                const event = new Event(id, start, end, summary, description, location, contact, reference);
+
+                events.unshift(event);
               });
 
               cbSuccess(events);
