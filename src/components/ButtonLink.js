80db59622f34afcc72c5d3c4c11f8b8c0da0b461
navacar <55153165+navacar@users.noreply.github.com>
2020-02-26 22:50:53 +0200

    Updated

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import './ButtonLink.css'
+import ButtonLinkImages from './ButtonLinkImages';
 
 function ButtonLink({ to, icon, title, style }) {
+  
   return (
     <Link className="link" to={to} style={style}>
-      <img src={icon} alt="le-icon" className="link__image_for_navigation" />
+      {/* <img src={icon} alt="le-icon" className="link__image_for_navigation" /> */}
+      <ButtonLinkImages 
+        icon = {icon}
+      />
       <span className="link__text"> {title} </span>
     </Link>
   ) 
