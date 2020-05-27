2891de8326ba48f71d62b95327cc5e94eeb09901
Vadim Cpp <vadim.cpp@gmail.com>
2020-05-27 10:58:10 +0200

    157 add alt option to components

--- src/components/ButtonLink.js
+++ src/components/ButtonLink.js
@@ -2,14 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './ButtonLink.css'
 
-function ButtonLink({ to, icon, title, style }) {
+function ButtonLink({ to, icon, alt, title, style }) {
   let CName = "link_image_welcome_list";
   if (icon === "/icons/icon_arrow_back.png"){
       CName = "link_image_for_navigation"    
   }
   return (
     <Link className="link" to={to} style={style}>
-      <img src={icon} alt="le-icon" className={CName} />
+      <img src={icon} alt={alt || "le-icon"} className={CName} />
       <span className="link__text"> {title} </span>
     </Link>
   ) 
