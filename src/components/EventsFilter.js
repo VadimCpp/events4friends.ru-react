c3604ce6051ce56a0e69724b716d3accc6f16ca1
Vadim Cpp <vadim.cpp@gmail.com>
2020-06-08 19:55:21 +0200

    #163 add logic to EventsFilter component

--- src/components/EventsFilter.js
+++ src/components/EventsFilter.js
@@ -1,17 +1,51 @@
 import React, { Component } from 'react';
 import './EventsFilter.css';
 
+const EventsFilterType = {
+  Upcoming: 'UPCOMING_EVENTS',
+  Past: 'PAST_EVENTS',
+  // TODO: add more types here
+};
+
 class EventsFilter extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      filterType: EventsFilterType.Upcoming,
+    };
   }
 
   render() {
+    const { filterType } = this.state;
+
     return (
       <div className="events-filter">
         <span className="events-filter__label">Фильтр:</span>
-        <button className="events-filter__button events-filter__button--disabled">Предстоящие</button>
-        <button className="events-filter__button">Прошедшие</button>
+        <button
+          className={
+            filterType === EventsFilterType.Upcoming
+            ? "events-filter__button events-filter__button--disabled"
+            : "events-filter__button"
+          }
+          onClick={() => {
+            this.setState({ filterType: EventsFilterType.Upcoming })
+          }}
+        >
+          Предстоящие
+        </button>
+        <button
+          className={
+            filterType === EventsFilterType.Past
+            ? "events-filter__button events-filter__button--disabled"
+            : "events-filter__button"
+          }
+          onClick={() => {
+            this.setState({ filterType: EventsFilterType.Past })
+          }}
+        >
+          Прошедшие
+        </button>
       </div>
     )
   }
