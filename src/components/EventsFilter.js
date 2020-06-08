890eec647d50d9b5f95efdfd13e8a7b5712e186f
Vadim Cpp <vadim.cpp@gmail.com>
2020-06-08 20:12:17 +0200

    #163 convert EventsFilter to functional component

--- src/components/EventsFilter.js
+++ src/components/EventsFilter.js
@@ -1,54 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './EventsFilter.css';
 
-class EventsFilter extends Component {
-  constructor(props) {
-    super(props);
-  }
+function EventsFilter(props) {
+  const { onFilterTypeChange, filterType, upcoming, past } = props;
 
-  render() {
-    const { onFilterTypeChange, filterType, upcoming, past } = this.props;
-
-    return (
-      <div className="events-filter">
-        <span className="events-filter__label">Фильтр:</span>
-        <button
-          className={
-            filterType === upcoming
-            ? "events-filter__button events-filter__button--disabled"
-            : "events-filter__button"
-          }
-          onClick={() => {
-            if (filterType !== upcoming) {
-              this.setState({ filterType: upcoming });
-              if (onFilterTypeChange) {
-                onFilterTypeChange(upcoming);
-              }
+  return (
+    <div className="events-filter">
+      <span className="events-filter__label">Фильтр:</span>
+      <button
+        className={
+          filterType === upcoming
+          ? "events-filter__button events-filter__button--disabled"
+          : "events-filter__button"
+        }
+        onClick={() => {
+          if (filterType !== upcoming) {
+            if (onFilterTypeChange) {
+              onFilterTypeChange(upcoming);
             }
-          }}
-        >
-          Предстоящие
-        </button>
-        <button
-          className={
-            filterType === past
-            ? "events-filter__button events-filter__button--disabled"
-            : "events-filter__button"
           }
-          onClick={() => {
-            if (filterType !== past) {
-              this.setState({ filterType: past });
-              if (onFilterTypeChange) {
-                onFilterTypeChange(past);
-              }
+        }}
+      >
+        Предстоящие
+      </button>
+      <button
+        className={
+          filterType === past
+          ? "events-filter__button events-filter__button--disabled"
+          : "events-filter__button"
+        }
+        onClick={() => {
+          if (filterType !== past) {
+            if (onFilterTypeChange) {
+              onFilterTypeChange(past);
             }
-          }}
-        >
-          Прошедшие
-        </button>
-      </div>
-    )
-  }
+          }
+        }}
+      >
+        Прошедшие
+      </button>
+    </div>
+  )
 };
 
 export default EventsFilter;
