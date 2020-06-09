c0ddd7adf1c883a50da03a17b41f604b42087c0c
unknown <aluminaze@gmail.com>
2020-06-09 15:51:47 +0200

    #164 convert ServiceSort to functional component

--- src/components/ServiceSort.js
+++ src/components/ServiceSort.js
@@ -1,65 +1,43 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './ServiceSort.css';
 
+let ServiceSort = (props) => {
+    const { sortType, sortByName, sortByPrice, sortByService, onSortTypeChange } = props;
 
-class ServiceSort extends Component {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-        const { sortType, sortByName, sortByPrice, sortByService, onSortTypeChange } = this.props;
-
-        return (
-            <div className="container service-sort">
-                <span className="service-sort__label">Сортировка:</span>
-                <button
-                    className={
-                        sortType === sortByName
-                            ? "service-sort__button service-sort__button--disabled"
-                            : "service-sort__button"
-                    }
-                    onClick={() => {
-                        if (sortType !== sortByName) {
-                            this.setState({ sortType: sortByName })
-                            if (onSortTypeChange) onSortTypeChange(sortByName)
-                        }
-                    }}
-                >
-                    Имя
-                </button>
-                <button
-                    className={
-                        sortType === sortByService
-                            ? "service-sort__button service-sort__button--disabled"
-                            : "service-sort__button"
-                    }
-                    onClick={() => {
-                        if (sortType !== sortByService) {
-                            this.setState({ sortType: sortByService })
-                            if (onSortTypeChange) onSortTypeChange(sortByService)
-                        }
-                    }}
-                >
-                    Услуга
-                </button>
-                <button
-                    className={sortType === sortByPrice
+    return (
+        <div className="container service-sort">
+            <span className="service-sort__label">Сортировка:</span>
+            <button
+                className={
+                    sortType === sortByName
                         ? "service-sort__button service-sort__button--disabled"
                         : "service-sort__button"
-                    }
-                    onClick={() => {
-                        if (sortType !== sortByPrice) {
-                            this.setState({ sortType: sortByPrice })
-                            if (onSortTypeChange) onSortTypeChange(sortByPrice)
-                        }
-                    }}
-                >
-                    Цена
-                </button>
-            </div>
-        )
-    }
+                }
+                onClick={() => (sortType !== sortByName && onSortTypeChange) ? onSortTypeChange(sortByName) : false}
+            >
+                Имя
+            </button>
+            <button
+                className={
+                    sortType === sortByService
+                        ? "service-sort__button service-sort__button--disabled"
+                        : "service-sort__button"
+                }
+                onClick={() => (sortType !== sortByService && onSortTypeChange) ? onSortTypeChange(sortByService) : false}
+            >
+                Услуга
+            </button>
+            <button
+                className={sortType === sortByPrice
+                    ? "service-sort__button service-sort__button--disabled"
+                    : "service-sort__button"
+                }
+                onClick={() => (sortType !== sortByPrice && onSortTypeChange) ? onSortTypeChange(sortByPrice) : false}
+            >
+                Цена
+            </button>
+        </div>
+    )
 }
 
 export default ServiceSort;
