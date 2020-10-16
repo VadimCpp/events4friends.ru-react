4cc765930095d5272ef758fe1e0e0029dc29d017
Sergey <vertigomaill@gmail.com>
2020-10-16 23:56:12 +0300

    #199 add useContext

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import { withRouter } from 'react-router-dom';
 import Button from '../components/Button';
 import ButtonLink from '../components/ButtonLink';
@@ -13,6 +13,8 @@ const LoginView = ({ history }) => {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
 
+  const authContext = useContext(AuthContext);
+
   const handleLoginChange = e => {
     setLogin(e.target.value);
   };
@@ -21,89 +23,85 @@ const LoginView = ({ history }) => {
     setPassword(e.target.value);
   };
 
+  const { user, signIn } = authContext;
+  let userName = null;
+  let userAuthorized = false;
+  if (user) {
+    const { isAnonymous, displayName } = user;
+    if (isAnonymous) {
+      userName = 'Аноним';
+    } else {
+      userName = displayName;
+      if (!userName) {
+        history.push('/profile');
+      }
+      userAuthorized = true;
+    }
+  }
+
   return (
     <div className="loginview">
       <div className="loginview__block">
-        <AuthContext.Consumer>
-          {({ user, signIn }) => {
-            let userName = null;
-            let userAuthorized = false;
-            if (user) {
-              const { isAnonymous, displayName } = user;
-              if (isAnonymous) {
-                userName = 'Аноним';
-              } else {
-                userName = displayName;
-                if (!userName) {
-                  history.push('/profile');
-                }
-                userAuthorized = true;
-              }
-            }
-            return (
-              <div className="container container-center">
-                {userAuthorized ? (
-                  <div>
-                    <p>
-                      <span>Вход выполнен, {userName}! </span>
-                    </p>
-                    <ButtonLink
-                      to="/"
-                      icon="/icons/icon_arrow_back.svg"
-                      title="Вернуться на экран приветствия"
-                      style={{
-                        width: 200,
-                        display: 'block',
-                        marginRight: 'auto',
-                        marginLeft: 'auto',
-                        marginBottom: 26,
-                        borderColor: 'rgba(77, 77, 77, .2)',
-                        borderRadius: '48px',
-                      }}
-                    />
-                  </div>
-                ) : (
-                  <div>
-                    <div className="textinput">
-                      <label>
-                        <span className="textinput__label">E-mail:</span>
-                        <input
-                          className="textinput__input"
-                          type="email"
-                          id="login"
-                          name="login"
-                          value={login}
-                          onChange={handleLoginChange}
-                        />
-                      </label>
-                    </div>
-                    <div className="textinput">
-                      <label>
-                        <span className="textinput__label">Пароль:</span>
-                        <input
-                          className="textinput__input"
-                          type="password"
-                          id="password"
-                          name="password"
-                          value={password}
-                          onChange={handlePasswordChange}
-                        />
-                      </label>
-                    </div>
-                    <Button
-                      onPress={() => {
-                        signIn(login, password);
-                      }}
-                      icon="/icons/icon_login.svg"
-                    >
-                      Войти
-                    </Button>
-                  </div>
-                )}
+        <div className="container container-center">
+          {userAuthorized ? (
+            <div>
+              <p>
+                <span>Вход выполнен, {userName}! </span>
+              </p>
+              <ButtonLink
+                to="/"
+                icon="/icons/icon_arrow_back.svg"
+                title="Вернуться на экран приветствия"
+                style={{
+                  width: 200,
+                  display: 'block',
+                  marginRight: 'auto',
+                  marginLeft: 'auto',
+                  marginBottom: 26,
+                  borderColor: 'rgba(77, 77, 77, .2)',
+                  borderRadius: '48px',
+                }}
+              />
+            </div>
+          ) : (
+            <div>
+              <div className="textinput">
+                <label>
+                  <span className="textinput__label">E-mail:</span>
+                  <input
+                    className="textinput__input"
+                    type="email"
+                    id="login"
+                    name="login"
+                    value={login}
+                    onChange={handleLoginChange}
+                  />
+                </label>
               </div>
-            );
-          }}
-        </AuthContext.Consumer>
+              <div className="textinput">
+                <label>
+                  <span className="textinput__label">Пароль:</span>
+                  <input
+                    className="textinput__input"
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
+                </label>
+              </div>
+              <Button
+                onPress={() => {
+                  signIn(login, password);
+                }}
+                icon="/icons/icon_login.svg"
+              >
+                Войти
+              </Button>
+            </div>
+          )}
+        </div>
       </div>
       <div className="loginview__block">
         <div className="container container-center">
