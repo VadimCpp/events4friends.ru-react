d2a5d8a1f5d0641e3320b7bdad3de3bc55daa601
Sergey <vertigomaill@gmail.com>
2020-10-20 13:33:14 +0300

    #199 refactor: ServicesView to functional component

--- src/views/ServicesView.js
+++ src/views/ServicesView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useContext } from 'react';
 import ServiceCard from '../components/ServiceCard';
 import ButtonLink from '../components/ButtonLink';
 import { DataContext } from '../context/DataContext';
@@ -11,15 +11,11 @@ const ServiceSortingType = {
   SortByPrice: 'SORT_BY_PRICE',
 };
 
-class ServicesView extends Component {
-  constructor(props) {
-    super(props);
+const ServicesView = () => {
+  const dataContext = useContext(DataContext);
+  const [sortType, setSortType] = useState(ServiceSortingType.SortByService);
 
-    this.state = { sortType: ServiceSortingType.SortByService };
-  }
-
-  displayService(service) {
-    const { sortType } = this.state;
+  const displayService = service => {
     let highlightName = false;
 
     if (sortType === 'SORT_BY_NAME') {
@@ -35,84 +31,77 @@ class ServicesView extends Component {
         <ServiceCard service={service} highlightName={highlightName} />
       </div>
     );
-  }
+  };
 
-  render() {
-    const { sortType } = this.state;
+  const getSortedServices = () => {
+    const { services } = dataContext;
+    let sortedServices = [...services];
 
-    return (
-      <div className="main-view">
-        <div>
-          <ButtonLink
-            to="/"
-            icon="/icons/icon_arrow_back.svg"
-            title="На главную"
-            style={{
-              width: 175,
-              display: 'block',
-              marginRight: 'auto',
-              marginLeft: 'auto',
-              marginBottom: 10,
-              borderColor: 'rgba(77, 77, 77, .2)',
-              borderRadius: '48px',
-            }}
-          />
-        </div>
+    if (sortType === ServiceSortingType.SortByName) {
+      sortedServices = sortedServices.sort((a, b) => {
+        return a.name ? a.name.localeCompare(b.name) : 0;
+      });
+    } else if (sortType === ServiceSortingType.SortByService) {
+      sortedServices = sortedServices.sort((a, b) => {
+        return a.service ? a.service.localeCompare(b.service) : 0;
+      });
+    } else if (sortType === ServiceSortingType.SortByPrice) {
+      sortedServices = sortedServices.sort((a, b) => {
+        if (a.isFree && b.isFree) {
+          return 0;
+        }
+        if (a.isFree) {
+          return -1;
+        }
+        if (b.isFree) {
+          return 1;
+        }
+        if (a.price && b.price) {
+          return a.price < b.price ? -1 : 0;
+        }
+        if (a.price && !b.price) {
+          return -1;
+        }
+        if (a.price && !b.price) {
+          return 1;
+        }
+        return 0;
+      });
+    }
 
-        <ServiceSort
-          onSortTypeChange={value => this.setState({ sortType: value })}
-          sortType={sortType}
-          sortByName={ServiceSortingType.SortByName}
-          sortByService={ServiceSortingType.SortByService}
-          sortByPrice={ServiceSortingType.SortByPrice}
+    return sortedServices.map(service => displayService(service));
+  };
+
+  return (
+    <div className="main-view">
+      <div>
+        <ButtonLink
+          to="/"
+          icon="/icons/icon_arrow_back.svg"
+          title="На главную"
+          style={{
+            width: 175,
+            display: 'block',
+            marginRight: 'auto',
+            marginLeft: 'auto',
+            marginBottom: 10,
+            borderColor: 'rgba(77, 77, 77, .2)',
+            borderRadius: '48px',
+          }}
         />
-
-        <div className="pt-3">
-          <DataContext.Consumer>
-            {({ services }) => {
-              let sortedServices = [...services];
-
-              if (sortType === ServiceSortingType.SortByName) {
-                sortedServices = sortedServices.sort((a, b) => {
-                  return a.name ? a.name.localeCompare(b.name) : 0;
-                });
-              } else if (sortType === ServiceSortingType.SortByService) {
-                sortedServices = sortedServices.sort((a, b) => {
-                  return a.service ? a.service.localeCompare(b.service) : 0;
-                });
-              } else if (sortType === ServiceSortingType.SortByPrice) {
-                sortedServices = sortedServices.sort((a, b) => {
-                  if (a.isFree && b.isFree) {
-                    return 0;
-                  }
-                  if (a.isFree) {
-                    return -1;
-                  }
-                  if (b.isFree) {
-                    return 1;
-                  }
-                  if (a.price && b.price) {
-                    return a.price < b.price ? -1 : 0;
-                  }
-                  if (a.price && !b.price) {
-                    return -1;
-                  }
-                  if (a.price && !b.price) {
-                    return 1;
-                  }
-                  return 0;
-                });
-              }
-
-              return sortedServices.map(service =>
-                this.displayService(service),
-              );
-            }}
-          </DataContext.Consumer>
-        </div>
       </div>
-    );
-  }
-}
+
+      <ServiceSort
+        onSortTypeChange={value => setSortType(value)}
+        sortType={sortType}
+        sortByName={ServiceSortingType.SortByName}
+        sortByService={ServiceSortingType.SortByService}
+        sortByPrice={ServiceSortingType.SortByPrice}
+      />
+
+      <div className="pt-3">{getSortedServices()}</div>
+    </div>
+  );
+};
 
 export default ServicesView;
