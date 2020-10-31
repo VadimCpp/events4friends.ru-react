5e5afef0a777377d9b2a5a7cb5b5e3686aee0646
Sergey <vertigomaill@gmail.com>
2020-10-31 23:49:03 +0300

    add ServiceForm

---
+++ src/components/ServiceForm/ServiceForm.js
@@ -0,0 +1,219 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { ReachTextEditor } from '../RichTextEditor';
+import Button from '../Button';
+import { verify, serviceInitState } from './helper';
+
+const ServiceForm = ({ defaultService, onSave = () => {} }) => {
+  const history = useHistory();
+  const [service, updateServiceValue] = useState(serviceInitState);
+  const [updatingService, setUpdatingService] = useState(false);
+
+  useEffect(() => {
+    if (defaultService) {
+      updateServiceValue({ ...serviceInitState, ...defaultService });
+    }
+  }, [defaultService]);
+
+  const saveHandler = () => {
+    setUpdatingService(true);
+
+    if (!verify(service)) {
+      console.warn('verify fail');
+      setUpdatingService(false);
+      return;
+    }
+
+    onSave(service, docId => {
+      if (docId) {
+        console.info('Service updated successfully, open it');
+        history.push(`/service/${docId}`);
+      } else {
+        setUpdatingService(false);
+        alert(
+          'Не удалось изменить услугу. Пожалуйста, обратитесь в службу поддержки.',
+        );
+      }
+    });
+  };
+
+  const handlerChange = key => ({ target }) => {
+    const { value } = target;
+    updateServiceValue({ ...service, [key]: value });
+  };
+
+  const handlerDescriptionChange = val => {
+    updateServiceValue({ ...service, description: val });
+  };
+
+  const handleIsFreeChange = isFree => () => {
+    if (isFree === true) {
+      updateServiceValue({ ...service, price: '', isFree });
+    } else {
+      updateServiceValue({ ...service, isFree });
+    }
+  };
+
+  return (
+    <div className="newserviceview">
+      <div className="textinput">
+        <label>
+          <p className="text-left">Название услуги:</p>
+          <input
+            className="textinput__input"
+            type="text"
+            name="service"
+            value={service.service}
+            onChange={handlerChange('service')}
+          />
+        </label>
+      </div>
+
+      <div className="textinput">
+        <label>
+          <p className="text-left">Имя того, кто оказывает услугу:</p>
+          <input
+            className="textinput__input"
+            type="text"
+            name="name"
+            value={service.name}
+            onChange={handlerChange('name')}
+          />
+        </label>
+      </div>
+
+      <div className="textinput">
+        <p className="text-left">Полное описание:</p>
+        <div className="rte-container">
+          <ReachTextEditor
+            description={service.description}
+            onChange={handlerDescriptionChange}
+          />
+        </div>
+      </div>
+
+      <div className="textinput">
+        <p className="text-left">Услуга оказывается платно?</p>
+        <p>
+          <label>
+            <span className="text-left">Да</span>
+            <input
+              className="textinput__input"
+              type="radio"
+              name="isFree"
+              checked={!service.isFree}
+              onChange={handleIsFreeChange(false)}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            <span className="text-left">Нет</span>
+            <input
+              className="textinput__input"
+              type="radio"
+              name="isFree"
+              checked={service.isFree}
+              onChange={handleIsFreeChange(true)}
+            />
+          </label>
+        </p>
+      </div>
+      <>
+        {!service.isFree && (
+          <div className="textinput">
+            <label>
+              <p className="text-left">Укажите стоимость услуги в рублях:</p>
+              <input
+                className="textinput__input"
+                type="text"
+                name="price"
+                value={service.price}
+                onChange={handlerChange('price')}
+                disabled={service.isFree}
+              />
+            </label>
+          </div>
+        )}
+      </>
+
+      <div className="textinput">
+        <label>
+          <p className="text-left">Cсылка на сайт:</p>
+          <input
+            className="textinput__input"
+            type="text"
+            name="website"
+            value={service.website}
+            onChange={handlerChange('website')}
+          />
+        </label>
+      </div>
+
+      <div className="textinput">
+        <label>
+          <p className="text-left">Ссылка на инстаграм:</p>
+          <input
+            className="textinput__input"
+            type="text"
+            name="instagram"
+            value={service.instagram}
+            onChange={handlerChange('instagram')}
+          />
+        </label>
+      </div>
+
+      <div className="textinput">
+        <label>
+          <p className="text-left">Номер в WhatsApp в формате 7XXX1234567:</p>
+          <input
+            className="textinput__input"
+            type="text"
+            name="whatsapp"
+            value={service.whatsapp}
+            onChange={handlerChange('whatsapp')}
+          />
+        </label>
+      </div>
+
+      <div className="textinput">
+        <label>
+          <p className="text-left">ID пользователя в телеграм:</p>
+          <input
+            className="textinput__input"
+            type="text"
+            name="telegram"
+            value={service.telegram}
+            onChange={handlerChange('telegram')}
+          />
+        </label>
+      </div>
+
+      <div className="textinput">
+        <label>
+          <p className="text-left">Ссылка ВКонтакте:</p>
+          <input
+            className="textinput__input"
+            type="text"
+            name="vkontakte"
+            value={service.vkontakte}
+            onChange={handlerChange('vkontakte')}
+          />
+        </label>
+      </div>
+
+      {updatingService ? (
+        <div>
+          <p>Сохраняем услугу...</p>
+        </div>
+      ) : (
+        <Button onPress={saveHandler} icon="/icons/icon_save.svg">
+          Сохранить
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default ServiceForm;
---
+++ src/components/ServiceForm/index.js
@@ -0,0 +1,96 @@
+import React from 'react';
+import ButtonLink from '../ButtonLink';
+import ButtonExternalLink from '../ButtonExternalLink';
+
+import ServiceForm from './ServiceForm';
+import './ServiceForm.css';
+
+const ServiceFormView = ({
+  editMode = true,
+  service,
+  isAuth,
+  onSave = () => {},
+}) => {
+  const formContent = isAuth ? (
+    <ServiceForm defaultService={service} onSave={onSave} editMode={editMode} />
+  ) : (
+    <div>
+      <p>Для того, чтобы редактировать услуги, выполните вход</p>
+    </div>
+  );
+
+  return (
+    <div>
+      <div>
+        <ButtonLink
+          to="/services"
+          icon="/icons/icon_arrow_back.svg"
+          title="К списку"
+          style={{
+            width: 155,
+            display: 'block',
+            marginRight: 'auto',
+            marginLeft: 'auto',
+            marginBottom: 10,
+            borderColor: 'rgba(77, 77, 77, .2)',
+            borderRadius: '48px',
+          }}
+        />
+      </div>
+
+      {formContent}
+
+      <div className="border-top">
+        <div className="container container-center pt-4 pb-5">
+          <p>
+            У вас есть вопросы о том, как редактировать услугу? Задайте вопрос в
+            чате:
+          </p>
+          <ButtonExternalLink
+            href="tg://resolve?domain=events4friends"
+            icon="/icons/telegram.svg"
+            alt="telegram"
+            style={{
+              borderColor: '#139BD0',
+              margin: 8,
+            }}
+          />
+          <ButtonExternalLink
+            href="https://chat.whatsapp.com/DWUaZ1bsuxwJLALyvBYTt8"
+            icon="/icons/whatsapp.svg"
+            alt="whatsapp"
+            style={{
+              borderColor: '#57BB63',
+              margin: 8,
+            }}
+          />
+          <ButtonExternalLink
+            href="https://invite.viber.com/?g2=AQBA7jF9Y7%2BXBkqTI0PoYF%2BmnEMluxPdGZy8wJQ3PRPBLT%2BMeh344RxBuBUTVc6B"
+            icon="/icons/viber.svg"
+            alt="viber"
+            style={{
+              borderColor: '#7C519B',
+              margin: 8,
+            }}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default React.memo(ServiceFormView, (prev, next) => {
+  if (prev.service.id !== next.service.id) {
+    return false;
+  }
+
+  if (prev.service.contact !== next.service.contact) {
+    return false;
+  }
+
+  if (prev.isAuth !== next.isAuth) {
+    return false;
+  }
+
+  return true;
+});
