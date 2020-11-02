664bed8a04a5d664b3413ed322710cb2c9031377
Vadim Cpp <vadim.cpp@gmail.com>
2020-11-02 16:57:59 +0200

    #241 add comments to AuthContex.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,7 +1,15 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 
 const defaultData = {
-  userName: 'Anonymous',
+  user: undefined, // object or undefined - текущий авторизованный пользователь firebase
+  signIn: (email, password) => {}, // метод вход пользователя по логину и паролю
+  signOut: () => {}, // метод осуществляет выход пользователя из системы
+  updateProfile: displayName => {}, // метод обновляет имя пользователя
+  loadingStatuses: { // разные флаги загрузки
+    connectingToFirebase: false, // флаг, который обозначает процесс подключения к базе firebase
+    loadingEvents: false, // флаг, который означает процесс загрузки событий
+  },
 };
 
 export const AuthContext = React.createContext(defaultData);
