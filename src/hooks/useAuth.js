0596152a62c1a983505399b145d70a63b3259222
Тимоновский Александр <36565080+at31oldfox@users.noreply.github.com>
2020-11-14 17:58:38 +0300

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -2,11 +2,8 @@ import { useState, useEffect } from 'react';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
-const useAuth = () => {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const firebaseConfig = {
+// константа, можно/нужно вынести из функции, можно вынести из файла, например в .env
+const firebaseConfig = {
       apiKey: 'AIzaSyBjAQdqx3qkki7MVb6dd1eASw-0UGs2Bg0',
       authDomain: 'events4friends.firebaseapp.com',
       databaseURL: 'https://events4friends.firebaseio.com',
@@ -17,11 +14,14 @@ const useAuth = () => {
       measurementId: 'G-4T13RKFFSG',
     };
 
-    firebase.initializeApp(firebaseConfig);
-  }, []);
-
+const useAuth = () => {
+  const [user, setUser] = useState(null);
+// использование двух useEffect не гарантирует ожидания выполнения первого затем второго, весь код внутри будет объеденен React-ом,
+// поэтому нет смыла использовать два useEffect, минусы такого подхода я показывал в примере.
   useEffect(() => {
-    try {
+      try {
+      // мы используем try/catch что очень правильно, добавим сюда же выполнение firebase.initializeApp
+      firebase.initializeApp(firebaseConfig);
       firebase.auth().onAuthStateChanged(async aUser => {
         if (aUser) {
           setUser(aUser);
@@ -34,7 +34,7 @@ const useAuth = () => {
       console.error('Auth Error', error);
     }
   }, []);
-
+    
   return { user };
 };
 
