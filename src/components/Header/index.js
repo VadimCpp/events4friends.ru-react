36f3b3d00ff61ff9cc641f66ecae84349ce8c687
Sergey <vertigomaill@gmail.com>
2020-12-20 23:10:02 +0300

    #270 add new year logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { Link, withRouter } from 'react-router-dom';
-import HeaderLogo from './header_logo_small.png';
+import HeaderLogo from './images/header_logo_small.png';
+import NewYearHeaderLogo from './images/new_year_header_logo.png';
 import './Header.css';
 
-const Header = () => (
+const headerLogos = {
+  default: HeaderLogo,
+  newYear: NewYearHeaderLogo,
+};
+
+const Header = ({ logoType = 'default' }) => (
   <header className="header">
     <div className="header__first-col">
       <Link className="reset-link-style" to="/">
-        <img className="header__logo_big" src={HeaderLogo} alt="Header Logo" />
+        <img
+          className="header__logo_big"
+          src={headerLogos[logoType]}
+          alt="Header Logo"
+        />
         <img
           className="header__logo_small"
           src={HeaderLogo}
