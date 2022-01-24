718e24f6a86b91a4f51d0a8626db9d5a1bbe19e3
open-ssl <lukyanov.stas@mail.ru>
2022-01-24 23:24:11 +0200

    #384 check loading for the first opening events page

--- src/views/EventsView/index.js
+++ src/views/EventsView/index.js
@@ -32,20 +32,12 @@ const EventsView = ({ match, history }) => {
       // - если сообщество не найдено, отобразить NOT_FOUND
       //
       const aCommunity = communities.find(c => c.slug === slug);
+
       if (aCommunity) {
         setCommunity(aCommunity);
-      } else {
+      } else if (!loadingEvents) {
         // TODO: реализовать NOT_FOUND экран
         console.warn('TODO: реализовать NOT_FOUND экран');
-        //
-        // NOTE !
-        // при первой загрузке событий по прямой ссылке
-        // например ".../kgd/events"
-        // aCommunity пустая => директимся к WelcomeView в следующей строчке
-        // Перезагрузка странички решает проблему.
-        // Нужно либо прибить логику с переходом по NOT_FOUND
-        // либо принудительно заполнять aCommunity из базы как в getCommunities
-        //
         history.push('/');
       }
     } else {
