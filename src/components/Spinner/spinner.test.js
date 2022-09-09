54a479c5b9a6d503076dfe81027c7b65a713e67e
Vadym Kaninskyi <vadim.cpp@gmail.com>
2022-09-09 16:46:20 +0200

    #430 add tests for Spinner component

--- src/components/Spinner/spinner.test.js
+++ src/components/Spinner/spinner.test.js
@@ -3,10 +3,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Spinner from './index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+describe('Spinner component', () => {
+  it('Has correct structure', () => {
+    const div = document.createElement('div');
 
-  ReactDOM.render(<Spinner message="waiting" />, div);
+    ReactDOM.render(<Spinner message="N/A" />, div);
+    expect(div.hasChildNodes()).toBeTruthy();
+    expect(div.childNodes.length).toBe(1);
 
-  ReactDOM.unmountComponentAtNode(div);
+    const spinnerContainer = div.childNodes[0];
+    const svg = spinnerContainer.childNodes[0];
+    const title = spinnerContainer.childNodes[1];
+
+    expect(
+      spinnerContainer.classList.contains('spinner-container'),
+    ).toBeTruthy();
+    expect(spinnerContainer.childNodes.length).toBe(2);
+    expect(svg.classList.contains('spinner')).toBeTruthy();
+    expect(title.classList.contains('message')).toBeTruthy();
+
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('Display correct text', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<Spinner message="loading" />, div);
+    expect(div.innerHTML.includes('loading')).toBeTruthy();
+    ReactDOM.unmountComponentAtNode(div);
+
+    ReactDOM.render(<Spinner message="few words status" />, div);
+    expect(div.innerHTML.includes('few words status')).toBeTruthy();
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
