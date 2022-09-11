7ac3a1ddcc6aecb4cbce242b5fc5235482c7f5fb
Tatyana Mirzova <44976901+sealione1311@users.noreply.github.com>
2022-09-11 20:51:31 +0300

    #430 add storeBadgeComponent's tests

--- src/components/StoreBadge/storeBadge.test.js
+++ src/components/StoreBadge/storeBadge.test.js
@@ -6,8 +6,10 @@ import StoreBadge from './index';
 describe('StoreBadge component', () => {
   it('Has correct structure', () => {
     const div = document.createElement('div');
+    const href =
+      'https://apps.apple.com/us/app/events4friends-%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D1%81%D1%82%D0%B2%D0%BE/id1509893426';
 
-    ReactDOM.render(<StoreBadge platform="ios" />, div);
+    ReactDOM.render(<StoreBadge platform="ios" width="35" />, div);
     expect(div.hasChildNodes()).toBeTruthy();
     expect(div.childNodes.length).toBe(1);
 
@@ -15,7 +17,10 @@ describe('StoreBadge component', () => {
     expect(storeBadgeContainer.tagName).toBe('A');
     expect(storeBadgeContainer.childNodes.length).toBe(1);
     expect(storeBadgeContainer.childNodes[0].tagName).toBe('IMG');
-
+    expect(storeBadgeContainer.href).toBe(href);
+    expect(storeBadgeContainer.childNodes[0].style.length).toBe(2);
+    expect(storeBadgeContainer.childNodes[0].style[0]).toBe('width');
+    expect(storeBadgeContainer.childNodes[0].style[1]).toBe('height');
     ReactDOM.unmountComponentAtNode(div);
   });
 });
