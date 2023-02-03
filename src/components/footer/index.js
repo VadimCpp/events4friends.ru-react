fb984e3b6d513dfe253dbfbc73a23d658e7121ed
Vadym Kaninskyi <vadim.cpp@gmail.com>
2023-02-03 11:18:20 +0100

    #449 use spacing utility for flex elements

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -7,10 +7,10 @@ const Footer = () => {
     <div className="bg-sky-600">
       <div className="container mx-auto pt-8 pb-10 px-5">
         <p className="text-white text-lg pb-7">Попробуйте наше мобильное приложение</p>
-        <div className="flex flex-col md:flex-row justify-center align-middle">
+        <div className="flex flex-col md:flex-row space-y-5 md:space-x-5 md:space-y-0 justify-center align-middle">
           {
             STORE_BADGE_ITEMS.map(storeBadge => (
-              <div className='mx-auto md:mx-5 pb-5' key={storeBadge.platform} >
+              <div className='mx-auto md:mx-0' key={storeBadge.platform} >
                 <StoreBadge platform={storeBadge.platform} width={String(storeBadge.width)} />
               </div>
             ))
--- src/components/section/index.js
+++ src/components/section/index.js
@@ -6,8 +6,12 @@ const Section = ({ id, title, link, image, slug, description }) => {
 
   return (
     <section id={slug} className={even ? "bg-white" : "bg-sky-200"}>
-      <div className="container mx-auto pt-8 pb-10 px-5 md:flex">
-        <div className="md:flex-1 pr-5">
+      <div className={
+        even ? 
+        "container mx-auto pt-8 pb-10 px-5 md:flex md:space-x-5" :
+        "container mx-auto pt-8 pb-10 px-5 md:flex md:space-x-5 md:flex-row-reverse md:space-x-reverse"
+      }>
+        <div className="md:flex-1">
           <img
             className="w-full object-cover md:h-full rounded-lg"
             src={`img/sections/${image}`} 
@@ -15,21 +19,23 @@ const Section = ({ id, title, link, image, slug, description }) => {
           />
         </div>
         <div className="md:flex-1">
-          <p className='pt-7 md:pt-2'>
-            <span className="border-sky-700 border-2 border-dotted text-black p-2 px-4 rounded-md">
-              {id}
-            </span>
-          </p>
-          <h2 className="text-2xl font-semibold pt-5">
-            {title}
-          </h2>
-          <p className="text-lg pt-3">
-            {description}
-          </p>
-          <div className="pt-10 pb-4">
-            <a href={link} className="text-xl text-center text-white bg-sky-600 border-sky-700 p-3 pb-4 rounded-xl border-2 shadow-lg w-60 mt-auto">
-              Группа ВКонтакте
-            </a>
+          <div className={even ? "flex flex-col space-y-5" : "flex flex-col space-y-5 text-right"}>
+            <p className="pt-2">
+              <span className="border-sky-700 border-2 border-dotted text-black p-2 px-4 rounded-md">
+                {id}
+              </span>
+            </p>
+            <h2 className="text-2xl font-semibold">
+              {title}
+            </h2>
+            <p className="text-lg">
+              {description}
+            </p>
+            <div className="pt-3 pb-4">
+              <a href={link} className="text-xl text-center text-white bg-sky-600 border-sky-700 p-3 pb-4 rounded-xl border-2 shadow-lg w-60 mt-auto">
+                Группа ВКонтакте
+              </a>
+            </div>
           </div>
         </div>
       </div>
